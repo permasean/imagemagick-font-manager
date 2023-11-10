0392ca619b923b06d72dde349b0deb9a139afe65
Sean Hwang <seanphwang@Seans-MacBook-Pro-2.local>
2023-11-10 17:58:43 -0600

    make it more interfaceable

--- font_manager.py
+++ font_manager.py
@@ -2,35 +2,44 @@ from xml.etree import ElementTree
 import os
 import logging
 
+# gotta figure out logging
+
 class FontManager:
-    def add_font(self, font_name, font_filepath, font_format='ttf') -> None:
-        xml_paths = find_xml()
+   def add_font(self, font_name, font_filepath, font_format='ttf', 
+      xml_filename='type-ghostscript.xml', top_directory='/opt/homebrew/Cellar/imagemagick') -> None:
+      xml_paths = find_xml(xml_filename, top_directory)
 
-        if len(xml_paths) == 0:
-           raise RuntimeError('There was an error adding font.')
-        
-        xml_path = xml_paths[0]
-        etree = ElementTree.parse(xml_path)
-        root = etree.getroot()
-        typemap = etree.find('typemap') if root.tag != 'typemap' else root
+      if len(xml_paths) == 0:
+         raise RuntimeError('There was an error adding font.')
+      
+      xml_path = xml_paths[0]
 
-        if typemap.tag != 'typemap':
-           raise RuntimeError('Could not find typemap tag in XML')
+      with open(xml_path) as f:
+         data = f.read()
 
-        new_type = ElementTree.SubElement(typemap, 'type')
-        new_type.set('format', font_format)
-        new_type.set('name', font_name)
-        new_type.set('glyphs', font_filepath)
+      logging.info(data)
 
-        etree.write(xml_path)
-        print("Successfully added font.")
+      etree = ElementTree.parse(xml_path)
+      root = etree.getroot()
+      typemap = etree.find('typemap') if root.tag != 'typemap' else root
 
-def find_xml(xml_filename='type-ghostscript.xml', 
-    top_directory='/opt/homebrew/Cellar/imagemagick') -> str:
+      if typemap.tag != 'typemap':
+         raise RuntimeError('Could not find typemap tag in XML')
+
+      new_type = ElementTree.SubElement(typemap, 'type')
+      new_type.set('format', font_format)
+      new_type.set('name', font_name)
+      new_type.set('glyphs', font_filepath)
+
+      etree.write(xml_path)
+      logging.debug("Successfully added font: " + font_name)
+
+def find_xml(xml_filename, top_directory) -> str:
     # currently only recognizes homebrew-installed imagemagick
     result = []
     for root, dir, files in os.walk(top_directory):
       if xml_filename in files:
+         logging.info(os.path.join(root, xml_filename))
          result.append(os.path.join(root, xml_filename))
 
     if len(result) == 0:
@@ -45,4 +54,4 @@ def find_xml(xml_filename='type-ghostscript.xml',
 
 if __name__ == "__main__":
    font_manager = FontManager()
-   font_manager.add_font('Montserrat ExtraBold', '/Users/seanphwang/Library/Fonts/Montserrat-ExtraBold.ttf')
+   font_manager.add_font('Lato Bold', '/Users/seanphwang/Projects/imagemagick-font-manager/Lato-Bold.ttf')
